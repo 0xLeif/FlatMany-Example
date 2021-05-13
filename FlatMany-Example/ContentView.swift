1c9021d4a8f52bdb57a357e7dc14a5a0716f31c5
Zach Eriksen <zmeriksen@icloud.com>
2021-05-13 13:46:15 -0500

    Initial Commit

---
+++ FlatMany-Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlatMany-Example
+//
+//  Created by Leif on 5/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlatMany-Example/FlatMany_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlatMany_ExampleApp.swift
+//  FlatMany-Example
+//
+//  Created by Leif on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct FlatMany_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
