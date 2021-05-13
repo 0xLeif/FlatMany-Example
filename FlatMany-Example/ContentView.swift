55c58377879cc501c424998a4fd9aacab94bb68f
Zach Eriksen <zmeriksen@icloud.com>
2021-05-13 14:12:31 -0500

    init example

--- FlatMany-Example/ContentView.swift
+++ FlatMany-Example/ContentView.swift
@@ -6,11 +6,42 @@
 //
 
 import SwiftUI
+import Combine
+
+import FlatMany
+func absurd<A>(_ never: Never) -> A { }
+
+import DataObject
+import SURL
 
 struct ContentView: View {
+    @State private var task: AnyCancellable?
+    @State private var data: [DataObject] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        guard !data.isEmpty else {
+            return AnyView(
+                ProgressView()
+                    .onAppear {
+                        task = Just<[Int]>([Int](1 ... 100))
+                            .mapError(absurd)
+                            .flatMany { value in
+                                "https://jsonplaceholder.typicode.com/todos/\(value)".url!.get()
+                                    .map { DataObject($0.0) }
+                                    .eraseToAnyPublisher()
+                            }
+                            .sink(receiveCompletion: { _ in }, receiveValue: {
+                                data = $0
+                            })
+                    }
+            )
+        }
+        
+        return AnyView(
+            List(data, id: \.self) { value in
+                Text(value.description)
+            }
+        )
     }
 }
 
